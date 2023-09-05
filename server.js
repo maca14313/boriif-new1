adacefb43a616d53a847bb296b378c8e327a803e
muhammad <muhammad@muhammads-MacBook-Pro.local>
2023-09-05 16:33:08 +0300

    first commit3

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require ("express");
+const cors=require('cors');
+const cookieParser=require("cookie-parser")
+const mysql=require('mysql')
+const bcrypt=require('bcryptjs')
+const dotenv = require('dotenv').config();
+const jwt=require("jsonwebtoken")
+const path = require("path");
+
+
+
+const app =express()
+app.use(cors());
+app.use(cookieParser())
+app.use(express.json());
+
+/// CREATA USER 'me'@'localhost' IDENTIFIED BY 'password';
+/// CREATE USER 'someuser'@'localhost' IDENTIFIED BY 'somepassword';
+
+/*
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"0925090339",
+    database:"test_try",
+    charset : 'utf8mb4',
+  }) */
+  const db=mysql.createConnection({
+    host:"sql11.freemysqlhosting.net",
+    user:"sql11644520",
+    password:"JRgbfIDZQN",
+    database:"sql11644520",
+    /*connectionLimit: 50,
+      queueLimit: 0,
+      waitForConnection: true*/
+    //charset : 'utf8mb4_unicode_ci',
+  })
+
+
+  var del = db._protocol._delegateError;
+db._protocol._delegateError = function(err, sequence){
+  if (err.fatal) {
+    console.trace('fatal error: ' + err.message);
+  }
+  return del.call(this, err, sequence);
+};
+
+
+
+const postLesson= require('./routes/post-lesson')
+
+app.use('/membersdata',postLesson)
+
+
+app.listen(8000,()=>{
+    console.log('welcom-back')
+})
